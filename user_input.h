b74030b54883a6c82592aecc69200e91adbe6cb9
Mohamed Medhat <mmedhat94@gmail.com>
2018-06-30 15:24:30 +0200

    added comments to header file

--- user_input.h
+++ user_input.h
@@ -9,13 +9,15 @@
 #ifndef USER_INPUT_H_
 #define USER_INPUT_H_
 
-#define DDR_LED			DDRA
-#define PORT_LED		PORTA
-#define PIN_NUMBER_LED	1
 
-#define DDR_BUTTON		DDRD
-#define PIN_BUTTON		PIND
-#define PIN_NUMBER_BUTTON 2
+#define DDR_LED			DDRA	// Choose from DDRA, DDRB, DDRC or DDRD
+#define PORT_LED		PORTA	//Choose from PORTA, PORTB, PORTC or PORTD
+#define PIN_NUMBER_LED	1		//Choose the desired pin, 0,1,2 ....7
+
+#define DDR_BUTTON		DDRD	//For the program to operate properly using interrupts, leave as it is
+#define PIN_BUTTON		PIND	//For the program to operate properly using interrupts, leave as it is
+#define PIN_NUMBER_BUTTON 2		//For the program to operate properly using interrupts, leave as it is
+
 
 
 #endif /* USER_INPUT_H_ */
